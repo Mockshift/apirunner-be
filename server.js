b388a191a81ce44bb7f3b2ea24b7fd1fdca2795f
kenangunen <gunenkenan@gmail.com>
2025-05-31 21:38:39 +0300

    should fail

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello, World!');
 });
 
+const unused = 'block me';
+
 const port = 3000;
 app.listen(port, () => {
   console.log('Server is running on http://localhost:3000');
