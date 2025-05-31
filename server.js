8abaf1881983e2b5d893cf6c3080cb4230432ad0
kenangunen <gunenkenan@gmail.com>
2025-05-31 22:11:05 +0300

    feat: install commitlint AR-3

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello, World!');
 });
 
-const unused = 'block meee';
-
 const port = 3000;
 app.listen(port, () => {
   console.log('Server is running on http://localhost:3000');
